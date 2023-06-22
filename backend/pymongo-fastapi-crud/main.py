122b322d4a8bf54ff7e4a7f1a6d751735dd787c3
Kalib <idevforweb@gmail.com>
2023-06-22 18:35:21 -0400

    first commit

---
+++ backend/pymongo-fastapi-crud/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from dotenv import dotenv_values
+from pymongo import MongoClient
+from routes import router as book_router
+from fastapi.middleware.cors import CORSMiddleware
+
+config = dotenv_values(".env")
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
+
+
+@app.get('/new')
+def root():
+    return {"hello": "world"}
+
+
+app.include_router(book_router, tags=["books"], prefix="/book")
---
+++ frontend/svelte-frontend/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  // Source : https://www.sitepoint.com/svelte-fetch-data/
+  // Fetch Method 
+  
+  import { onMount } from "svelte";
+
+  const endpoint = "http://127.0.0.1:8000/book";
+
+  let posts = [];
+
+  onMount(async function () {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    posts = data;
+    console.log(posts);
+  });
+
+</script>
+
+{#each posts as key}
+  <div>
+    <p>{key.title}</p>
+    <p>{key.author}</p>
+  </div>
+{/each}
